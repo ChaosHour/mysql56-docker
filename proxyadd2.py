7bf11740a19e96d4b00d0ec9d28f1d4cb1fdc689
ChaosHour <KLarsen@reachlocal.com>
2023-11-28 19:47:18 -0800

    Updated proxyadd2.py

--- proxyadd2.py
+++ proxyadd2.py
@@ -10,25 +10,30 @@ def setup_logging():
 def connect_to_proxysql():
     return mysql.connector.connect(user='admin', password='admin', host='127.0.0.1', port=6032, charset='utf8')
 
+logging.basicConfig(level=logging.DEBUG)
+
 def get_rows(cnx):
     cursor = cnx.cursor()
     cursor.execute("SELECT * FROM stats.stats_mysql_query_digest WHERE digest_text LIKE '%SET NAMES latin1%'")
     rows = cursor.fetchall()
     cursor.close()
+    logging.debug(f"Fetched {len(rows)} rows")
     return rows
 
 def write_to_file(rows):
+    logging.debug(f"Writing {len(rows)} rows to file")
     with open('output.sql', 'w', encoding='utf-8') as f:
         rule_id = 1
         for row in rows:
             username = row[2]
             logging.info(f"Found 'SET NAMES latin1' for user: {username}")
-            f.write(f"INSERT INTO mysql_query_rules (rule_id, active, username, match_pattern, destination_hostgroup, apply) VALUES ({rule_id}, 1, '{username}', '^INSERT INTO chaos.|^UPDATE chaos.|^DELETE FROM chaos.', 9999, 1) ON DUPLICATE KEY UPDATE active=VALUES(active), username=VALUES(username), match_pattern=VALUES(match_pattern), destination_hostgroup=VALUES(destination_hostgroup), apply=VALUES(apply);\n")
+            f.write(f"REPLACE INTO mysql_query_rules (rule_id, active, username, match_pattern, destination_hostgroup, apply) VALUES ({rule_id}, 1, '{username}', '^INSERT INTO |^UPDATE |^DELETE FROM ', 9999, 1);\n")
             rule_id += 1
         f.write("LOAD MYSQL USERS TO RUNTIME;\n")
         f.write("LOAD MYSQL QUERY RULES TO RUNTIME;\n")
         f.write("SAVE MYSQL USERS TO DISK;\n")
         f.write("SAVE MYSQL QUERY RULES TO DISK;\n")
+
 
 def feed_sql_to_proxysql():
     os.system("mysql -h 127.0.0.1 -P 6032 -u admin -padmin < output.sql")
